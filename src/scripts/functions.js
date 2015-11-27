d14199230099c12e3f2c292d2583fe22cfdcf259
David Putney <david@davidputney.com>
2015-11-27 08:24:40 -0500

    First commit.

---
+++ src/scripts/functions.js
@@ -0,0 +1,11 @@
+//  this is a test
+
+var pageFunctions = {
+    intialize: function () {
+      console.log('works');
+      var self=this;
+      this.intializeWatchers(); //listens for clicks
+    },
+    intializeWatchers: function () {
+    }
+  };
