83d2dd4e1ceb811c92be86d01b6d1489e53edd13
David Putney <david@davidputney.com>
2015-12-26 09:40:30 -0500

    JS that loads style sheet and reworks the markup in embeds.

--- src/scripts/exclude/irotm_style_load.js
+++ src/scripts/exclude/irotm_style_load.js
@@ -15,20 +15,63 @@ var remoteLoader = {
 
       var entries = Array.prototype.slice.call(document.getElementsByClassName('machines-embed'));
 
-
-      console.log('foo', entries);
-
       entries.forEach (function (entries) {
         console.log(entries.innerHTML);
-        var container = document.createElement('div');
+        var container = document.createElement('article');
           container.setAttribute('class', 'machines-wrapper');
           container.innerHTML = entries.innerHTML;
 
           var parent = entries.parentNode;
+          var foo = entries.firstChild;
 
-          var foo = entries.childNode;
+          // removes headline and replaces with h3
+          var headline = document.createElement('h3');
+          headline.setAttribute('class', 'machines-embed-headline');
+          headline.innerText = foo.innerText;
 
-          console.log(foo);
+          container.firstChild.remove()
+          container.insertBefore(headline, container.firstChild);
+
+          var branding = document.createElement('figure');
+          branding.setAttribute('class', 'machines-embed-branding');
+
+          var brandingImage = document.createElement('IMG');
+          brandingImage.setAttribute('src', '/siteart/embed-branding.svg');
+          brandingImage.setAttribute('ALT', 'Inevitableriseofthemachines.com logo');
+          brandingImage.setAttribute('CLASS', 'machines-embed-image')
+
+          var brandingLink = document.createElement('A');
+          brandingLink.setAttribute('HREF', 'https://www.inevtiableriseofthemachines.com');
+          brandingLink.setAttribute('CLASS', 'machines-embed-branding-link');
+
+          brandingLink.appendChild(brandingImage);
+          branding.appendChild(brandingLink);
+
+          console.log('branding', branding);
+
+
+          container.insertBefore(branding, headline);
+
+          var blockquote = Array.prototype.slice.call(container.getElementsByTagName('BLOCKQUOTE'));
+          blockquote.forEach(function (blockquote) {
+            var styleBlockquote = document.createElement('blockquote');
+            styleBlockquote.setAttribute('class', 'machines-embed-blockquote');
+            styleBlockquote.innerText = blockquote.innerText;
+            container.insertBefore(styleBlockquote, blockquote);
+            blockquote.remove();
+          });
+
+          var paragraphs = Array.prototype.slice.call(container.getElementsByTagName('P'));
+          paragraphs.forEach(function (paragraphs) {
+            paragraphs.setAttribute('class', 'machines-embed-text');
+          });
+
+          var link = Array.prototype.slice.call(container.getElementsByTagName('A'));
+          link.forEach(function (link) {
+            if(!link.classList.contains('machines-embed-branding-link'))  {
+              link.setAttribute('class', 'machines-embed-link');
+            }
+          });
 
           parent.insertBefore(container, entries);
           entries.remove();
