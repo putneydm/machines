d01cddf2308d3d40478baeb2610d597ffcfe770e
David Putney <david@davidputney.com>
2016-02-24 09:06:02 -0500

    triggers search highlighter on entry if the entry url has a search term variable in it.

--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -36,6 +36,9 @@ var pageFunctions = {
         var self=this;
         var container = document.querySelector(".flip-card");
         container.addEventListener("click", self.detectClick, false);
+        if (searchTerm) {
+          self.termHighlighter(searchTerm);
+        }
     },
     initializeBlogPage: function () {
         var self=this;
